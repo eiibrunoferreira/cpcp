6e8607958a2e9e4015559e018e6ea0da5186be0c
eiibrunoferreira <brunoferreirabf2002@gmail.com>
2024-01-30 13:50:05 -0300

    formatação documento

--- script.js
+++ script.js
@@ -31,13 +31,34 @@ function soma() {
   const trocaX = document.querySelector(".trocaX").value;
   const trocaD = document.querySelector(".trocaD").value;
 
-  const somaSeg = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(segX * 1.4 + segD * 1.4);
-  const somaTer = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(terX * 1.4 + terD * 1.4);
-  const somaQua = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(quaX * 1.4 + quaD * 1.4);
-  const somaQui = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(quiX * 1.4 + quiD * 1.4);
-  const somaSex = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(sexX * 1.4 + sexD * 1.4);
-  const somaSab = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(sabX * 1.4 + sabD * 1.4);
-  const somaDom = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(domX * 1.4 + domD * 1.4);
+  const somaSeg = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(segX * 1.4 + segD * 1.4);
+  const somaTer = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(terX * 1.4 + terD * 1.4);
+  const somaQua = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(quaX * 1.4 + quaD * 1.4);
+  const somaQui = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(quiX * 1.4 + quiD * 1.4);
+  const somaSex = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(sexX * 1.4 + sexD * 1.4);
+  const somaSab = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(sabX * 1.4 + sabD * 1.4);
+  const somaDom = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(domX * 1.4 + domD * 1.4);
 
   const somatoria =
     segX * 1.4 +
@@ -55,22 +76,32 @@ function soma() {
     domX * 1.4 +
     domD * 1.4;
 
-  const somatotal = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(parseInt(somatoria) + parseInt(pendingAmount));
+  const somatotal = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(parseInt(somatoria) + parseInt(pendingAmount));
   const somatotall = parseInt(somatoria) + parseInt(pendingAmount);
 
-  const somaTroca = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(trocaX * 1.4 + trocaD * 1.4);
-  const somaTrocaa = (trocaX * 1.4 + trocaD * 1.4);
+  const somaTroca = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(trocaX * 1.4 + trocaD * 1.4);
+  const somaTrocaa = trocaX * 1.4 + trocaD * 1.4;
 
-  const somafinal = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(parseInt(somatotall) - parseInt(somaTrocaa));
+  const somafinal = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(parseInt(somatotall) - parseInt(somaTrocaa));
 
-
-  
   resultado.innerHTML = `
   <br>
   <h2>*👤${nameCliente.toUpperCase()}*<h2>
   <br>
   <br>
-  <h3>*🟥 VALOR PENDENTE ${Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(parseInt(pendingAmount))}*<h3>
+  <h3>*🟥 VALOR PENDENTE ${Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(parseInt(pendingAmount))}*<h3>
   <br>
   <br>
   <h3>*🔶SEGUNDA FEIRA*</h3>
